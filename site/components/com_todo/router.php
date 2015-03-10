17556c350bd8b30139948d5fe04ce71cbb34be93
Cameron Barr <cameron@timble.com>
2015-03-10 11:08:45 -0400

    re nooku/www.nooku.org#7 - simplify the backbone view behavior and the Todo model

--- site/components/com_todo/router.php
+++ site/components/com_todo/router.php
@@ -67,11 +67,6 @@ function TodoParseRoute($segments)
     if(isset($segments[1])) {
         $vars['id'] = $segments[1];
     }
-    // if the id is set, we want a `singular` view
-    if(isset($vars['id']))
-    {
-    //    $vars['view'] = KStringInflector::singularize($segments[0]);
-    }
 
     $menu      = JFactory::getApplication()->getMenu()->getActive();
     if (isset($menu->query['layout'])) {
